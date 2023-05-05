2fe840a5140d5276e06324225b05b9dcc62f4f2b
1981narayanvenna <132698783+1981narayanvenna@users.noreply.github.com>
2023-05-05 23:32:03 +0530

    Update stramlit_app.py
    Added pandas library

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('Breakfast Favorites')
@@ -7,3 +8,6 @@ streamlit.text("Idly & Vada")
 streamlit.text('🥑 Avacado Toast poori')
 streamlit.text('🐔 Biryani ')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
