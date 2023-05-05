6e2d0d68cb18dfbc96f613de2534e564d4ad3c10
1981narayanvenna <132698783+1981narayanvenna@users.noreply.github.com>
2023-05-05 23:22:12 +0530

    Update stramlit_app.py
    Added Breakfast Items

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text("Omega3 & Blueberry Juice")
+streamlit.text("Idly & Vada")
+streamlit.text('poori')
+
