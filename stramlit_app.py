3816f93cf535efee2fd838ff4cab3d5d569180c5
1981narayanvenna <132698783+1981narayanvenna@users.noreply.github.com>
2023-05-06 10:26:30 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -21,5 +21,8 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fr
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
+
